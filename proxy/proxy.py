2952b2b902f27f91aa338aa2a1e4f7c00756ce70
Haris Ibrahim <blucalvin@gmail.com>
2011-09-10 03:01:50 +0530

    Got IP and port

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -1,3 +1,7 @@
 import socket, SocketServer
+
+port = 8000
 local_hostname = socket.gethostname()
 print local_hostname
+server_address = (socket.gethostbyname(local_hostname), port)
+print server_address
